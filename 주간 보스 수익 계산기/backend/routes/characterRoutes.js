85a32254d8eb7a1a5b430c0c989c97b794e9a6d7
pwdab <soundno07@naver.com>
2025-01-14 20:15:56 +0900

    file updates
    주간 보스 수익 계산기
    - 삭제

--- 주간 보스 수익 계산기/backend/routes/characterRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getCharacterData } = require('../NexonOpenAPI/NexonOpenAPI');
-
-const router = express.Router();
-
-// GET /api/character/:nickname
-router.get('/:characterName', getCharacterData);
-
-module.exports = router;
